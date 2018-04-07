96aa0f7f40a650e3c6dc948871fe81105be42e88
IIIatterhand <38165794+IIIatterhand@users.noreply.github.com>
2018-04-07 22:28:18 +0300

    Add files via upload

---
+++ MyScript.js
@@ -0,0 +1,146 @@
+'use strict';
+
+let plan = `
+###############
+#             #
+#             #
+#             #
+#             #
+#             #
+###############`;
+
+plan = plan.trim().split('\n');
+
+let planW = plan[0].length;
+let planH = plan.length;
+
+let mageX = 3;
+let mageY = 2;
+
+function printPlan() {
+    let s = '';
+    for (let i = 0; i < planH; i++) {
+        for (let j = 0; j < planW; j++) {
+            if (j == mageX && i == mageY) {
+                s += 'M';
+            } else {
+                s += plan[i][j];
+            }
+        }
+        s += '\n';
+    }
+    console.log(s);
+}
+
+printPlan();
+
+function moveLeft() {
+    if (plan[mageY][mageX - 1] == ' ') {
+        mageX--;
+    }
+    printPlan();
+}
+
+function moveRight() {
+    if (plan[mageY][mageX + 1] == ' ') {
+        mageX++;
+    }
+    printPlan();
+}
+function moveUp() {
+    if (plan[mageY-1][mageX] == ' ') {
+        mageY--;
+    }
+    printPlan();
+}
+function moveDown() {
+    if (plan[mageY+1][mageX] == ' ') {
+        mageY++;
+    }
+    printPlan();
+}
+function moveDownRight() {
+    if (plan[mageY+1][mageX+1] == ' ') {
+        mageY++;
+        mageX++;
+    }
+    printPlan();
+}
+function moveDownLeft() {
+    if (plan[mageY+1][mageX-1] == ' ') {
+        mageY++;
+        mageX--;
+    }
+    printPlan();
+}
+function moveUpRight() {
+    if (plan[mageY-1][mageX+1] == ' ') {
+        mageY--;
+        mageX++;
+    }
+    printPlan();
+}
+function moveUpLeft() {
+    if (plan[mageY-1][mageX-1] == ' ') {
+        mageY--;
+        mageX--;
+    }
+    printPlan();
+}
+while (plan[mageY][mageX + 1] == ' ') {
+    moveRight();
+}
+//x=>4  y=>8
+//{x:4,y:8}
+let dir = {x: 13, y: 5}
+function move()
+{
+let dirX=dir.x;
+let dirY=dir.y; // Вытаскием координаты из строки dir вида {x:4,y:8}
+if (dirX!==mageX)
+{
+    if (Math.abs(dirY-mageY)>Math.abs(dirX-mageX)) // Если разница координат Y больше, чем разница координат X, то очевидно, что мы будем двигаться по y
+    {      
+        if (dirY>mageY) // Если dirY больше mageY - двигаемся вниз (связано с тем, что наша ось Y направлена вниз) 
+        {
+            moveDown();
+        }else if (dirY<mageY) //Если dirY меньше mageY - двигаемся наверх (см. выше)
+        {
+            moveUp();
+        } // в этом цикле мы никогда не попадаем на условие, что dirY=mageY, однако это покрывается следующей веткой условий
+    }
+    else if (Math.abs(dirY-mageY)<Math.abs(dirX-mageX)) //Если разница координат у меньше, чем разница координат х, то мы двигаемся по иксу (если разница координат y=0, то разница координат X больше В любом случае)
+    {
+        if (dirX>mageX) 
+        {
+            moveRight();
+        }else if (dirX<mageX) 
+        {
+            moveLeft();
+        }
+    }
+    else if (Math.abs(dirY-mageY)==Math.abs(dirX-mageX)) // Если их разница координат равна, то мы будем двигаться по диагонали. Нужно решить - в какую сторону нам идти - вправо вниз или вправо вверх
+    {
+        if (dirY>mageY) // Если dirY больше mageY - двигаемся по диагонали вниз
+        {
+            moveDownRight();
+        }else if (dirY<mageY) //Если dirY меньше mageY - двигаемся по диагонали вверх
+        {
+            moveUpRight();
+        } 
+    }
+}else if (dirX==mageX)
+{
+    if (dirY==mageY)
+    {
+
+    }else if (dirY>mageY)
+    {
+        moveDown();
+    }else 
+    {
+        moveUp();
+    } 
+}
+
+}
